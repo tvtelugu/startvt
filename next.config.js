74be3b0460f841c6e3928f8b9f1fb5b9d383dbe5
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2025-05-06 15:23:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
+module.exports = {
+  api: {
+    bodyParser: false,
+    externalResolver: true
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' }
+        ]
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
